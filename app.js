b38e4622fbdc1f78715853391b9888f8f6a9242a
Arun <coolarun477@gmail.com>
2017-08-14 09:14:49 -0700

    'first'

---
+++ app.js
@@ -0,0 +1,58 @@
+//MODULE
+
+var weatherApp = angular.module('weatherApp', ['ngRoute']);
+
+//SERVICES
+
+weatherApp.service('cityService',function(){
+    let self = this;
+    self.city = 'New York, NY';
+});
+
+
+
+//CONTROLLERS
+weatherApp.controller('homeController',['$scope','cityService',function($scope,cityService){
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city',function(){
+        cityService.city = $scope.city;
+    });
+    
+}]);
+
+weatherApp.controller('forecastController',['$scope','cityService','$http',function($scope,cityService,$http){
+    $scope.city = cityService.city;
+    
+    
+              
+        
+    
+  $http({
+      method:'GET',
+      url:"https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"?cnt=3,IN&APPID=03ff4754e1cc27ff1e2c4f59d1408f68"
+  }).then(function(response){
+      console.log(response);
+      $scope.weatherResult = response.data;
+      
+  },function(error){console.log(error)})
+                
+                
+   
+    
+}]);
+
+
+//ROUTES
+
+weatherApp.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl:'pages/home.html',
+        controller:'homeController'
+    })
+    .when('/forecast',{
+        templateUrl:'pages/forecast.html',
+        controller:'forecastController'
+    })
+});
